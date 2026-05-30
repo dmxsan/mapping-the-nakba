1cc5079142c7ec9ccab351c03249722039d8c93a
dmxsan <dimasmaulanaichsan@gmail.com>
2026-05-30 17:41:51 +0700

    add vercel web analytics

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue'
 import WebGISViewer from '@/components/WebGISViewer.vue'
 </script>
 
 <template>
   <WebGISViewer />
+  <Analytics />
 </template>
