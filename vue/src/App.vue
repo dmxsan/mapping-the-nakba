14d42761257f5844bca2a59e6f6845befc037bec
dmxsan <dimasmaulanaichsan@gmail.com>
2026-06-02 16:53:46 +0700

    refactor: use SpeedInsights Vue component instead of inject

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { inject } from '@vercel/analytics'
-import { injectSpeedInsights } from '@vercel/speed-insights'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 import WebGISViewer from '@/components/WebGISViewer.vue'
 
 inject()
-injectSpeedInsights()
 </script>
 
 <template>
   <WebGISViewer />
+  <SpeedInsights />
 </template>
